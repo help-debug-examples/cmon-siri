72edca3f6d84a404451a6cf31bdc2b81ef86e3c3
Derrick Showers <derrick@derrickshowers.com>
2018-12-14 14:39:45 -0500

    Initial commit

---
+++ CmonSiri/Screens/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  CmonSiri
+//
+//  Created by Derrick Showers on 12/14/18.
+//  Copyright © 2018 Derrick Showers. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
